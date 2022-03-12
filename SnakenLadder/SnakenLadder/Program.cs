a2e2bd01bd50635c33820cfeab323c2df2187725
kiran1902 <kiru2kumar@gmail.com>
2022-03-12 15:52:28 +0530

    Initial Commit-

---
+++ SnakenLadder/SnakenLadder/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int position = 0;
+Console.WriteLine("Player is at Start Position : " + position);
+Console.WriteLine("Single Player");
+Console.ReadLine();
+
