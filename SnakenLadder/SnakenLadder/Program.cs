b078ac15c80108e49e770d75c8c922d84240dafb
kiran1902 <kiru2kumar@gmail.com>
2022-03-12 18:30:22 +0530

    Palyer Reaches Winning Position 100

--- SnakenLadder/SnakenLadder/Program.cs
+++ SnakenLadder/SnakenLadder/Program.cs
@@ -6,29 +6,44 @@ const int Ladder = 1;
 const int Snake = 2;
 const int NoPlay = 3;
 
-
-Random random = new Random();      //initiatizlize the random class
-int DiceRolled = random.Next(7);   //class to generate randome number from 1 - 6
-Console.WriteLine("Number of times the dice was Rolled : " + DiceRolled);
-
 int PosPlayer = 0;
-Random random = new Random();   
-int ResultSnake = random.Next(4); //class to generate randome number from 1 - 3
+int Player = 1;
+while (PosPlayer < 100)
+{
+    Random random = new Random();      //initiatizlize the random class
+    int DiceRolled = random.Next(7);   //class to generate randome number from 1 - 6
+    Console.WriteLine("Number of times the dice was Rolled : " + DiceRolled);
 
-if (ResultSnake == Ladder)
-{
-    PosPlayer = PosPlayer + DiceRolled;
-    Console.WriteLine("Ladder");
-    Console.WriteLine("Player Current Position : " + PosPlayer);
-}
-else if (ResultSnake == Snake)
-{
-    PosPlayer = PosPlayer - DiceRolled;
-    Console.WriteLine("Snake");
-    Console.WriteLine("Player Current Position : " + PosPlayer);
-}
-else
-{
-    Console.WriteLine("No Play");
+
+ 
+    int ResultSnake = random.Next(4); //class to generate randome number from 1 - 3
+
+    if (ResultSnake == Ladder)
+    {
+        if (PosPlayer + DiceRolled <= 100)
+        {
+            PosPlayer = PosPlayer + DiceRolled;
+            Console.WriteLine("Ladder");
+            Console.WriteLine("Player Current Position : " + PosPlayer);
+        }
+    }
+    else if (ResultSnake == Snake)
+    {
+        if (PosPlayer + DiceRolled <= 0)
+        {
+            PosPlayer = 0;
+        }
+        else
+        {
+            PosPlayer = PosPlayer - DiceRolled;
+        }
+        Console.WriteLine("Snake");
+        Console.WriteLine("Player Current Position : " + PosPlayer);
+        
+    }
+    else
+    {
+        Console.WriteLine("No Play");
+    }
 }
 
