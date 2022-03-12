2baa365724634492060f66ba6b4170136700e543
kiran1902 <kiru2kumar@gmail.com>
2022-03-12 17:53:04 +0530

    Check Player Position

--- SnakenLadder/SnakenLadder/Program.cs
+++ SnakenLadder/SnakenLadder/Program.cs
@@ -1,12 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Weelcome To Paly snake And Ladder");
+Console.WriteLine("Welcome To play Snake And Ladder");
+
+//Declaring Constants
+const int Ladder = 1;
+const int Snake = 2;
+const int NoPlay = 3;
 
 
-Random random = new Random();
-int DiceRolled = random.Next(7);
+Random random = new Random();      //initiatizlize the random class
+int DiceRolled = random.Next(7);   //class to generate randome number from 1 - 6
 Console.WriteLine("Number of times the dice was Rolled : " + DiceRolled);
 
-int position = 0;
-Console.WriteLine("Player is at Start Position : " + position);
-Console.WriteLine("Single Player");
-Console.ReadLine();
+int PosPlayer = 0;
+int ResultSnake = random.Next(4); //class to generate randome number from 1 - 3
+
+if (ResultSnake == Ladder)
+{
+    PosPlayer = PosPlayer + DiceRolled;
+    Console.WriteLine("Ladder");
+    Console.WriteLine("Player Current Position : " + PosPlayer);
+}
+else if (ResultSnake == Snake)
+{
+    PosPlayer = PosPlayer - DiceRolled;
+    Console.WriteLine("Snake");
+    Console.WriteLine("Player Current Position : " + PosPlayer);
+}
+else
+{
+    Console.WriteLine("No Play");
+}
+
