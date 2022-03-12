1a4119a107f2d9678d16105c8d4acd6b9e04d02b
kiran1902 <kiru2kumar@gmail.com>
2022-03-12 17:22:05 +0530

    Number of time the dice was rolled

--- SnakenLadder/SnakenLadder/Program.cs
+++ SnakenLadder/SnakenLadder/Program.cs
@@ -1,7 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Weelcome To Paly snake And Ladder");
+
+
+Random random = new Random();
+int DiceRolled = random.Next(7);
+Console.WriteLine("Number of times the dice was Rolled : " + DiceRolled);
 
 int position = 0;
 Console.WriteLine("Player is at Start Position : " + position);
 Console.WriteLine("Single Player");
 Console.ReadLine();
-
